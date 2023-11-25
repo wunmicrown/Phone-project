4b58949b08709bcd06f6333e4f7d862daa2cfd0c
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-11-25 17:43:26 +0100

    added a script to load glo card

--- call.js
+++ call.js
@@ -280,3 +280,134 @@ const loadCardAirtel=()=> {
 }
 
 // END OF CODES TO LOAD AIRTEL CARD
+
+// BEGINNING OF CODES TO LOAD GLO CARD
+
+const loadCardGlo=()=> {
+    if (displaynumber.value.length == 21) {
+        const searchnetwork = JSON.parse(localStorage.getItem("Voucher"));
+        for (let q = 0; q < searchnetwork.length; q++) {
+            let searchingcode = searchnetwork[q].code;
+            if ((displaynumber.value.slice(0, 5) == "*311*") &&
+                (searchingcode.search(displaynumber.value.slice(5, 20)) != -1) &&
+                (displaynumber.value.slice(20, 21) == "*310#") &&
+                (searchnetwork[q].cardname == "Glo") &&
+                (searchnetwork[q].status == "notused")) {
+                searchnetwork[q].status = "used";
+                showbalance.style.display = "block";
+                selectsim.style.display = "none";
+                let getbal = parseInt(searchnetwork[q].cardamount);
+                let gettt = JSON.parse(localStorage.getItem("accountbal"));
+                gettt.global += getbal;
+                localStorage.setItem("accountbal", JSON.stringify(gettt));
+                displaybalance.innerHTML = "Recharge successful. Your account has been credited with GLO " + searchnetwork[q].cardamount + " . Your new account bal :₦" + gettt.global.toFixed(2);
+            } else if ((displaynumber.value.slice(0, 5) == "*311*") &&
+                (searchingcode.search(displaynumber.value.slice(5, 20)) != -1) &&
+                (displaynumber.value.slice(20, 21) == "*310#") &&
+                (searchnetwork[q].cardname == "Glo") &&
+                (searchnetwork[q].status == "used")) {
+                selectsim.style.display = "none";
+                showbalance.style.display = "block";
+                displaybalance.innerHTML = "This card has been loaded by a customer. Thanks!!!";
+            }
+            localStorage.setItem("Voucher", JSON.stringify(searchnetwork));
+        }
+    } else if (displaynumber.value.slice(0, 5) == "*310#") {
+        selectsim.style.display = "none";
+        showbalance.style.display = "block";
+        const displaybal = JSON.parse(localStorage.getItem("accountbal"));
+        const showbal = displaybal.global;
+        displaybalance.innerHTML = ("Your GLO account balance is ₦" + showbal.toFixed(2));
+    } else if ((displaynumber.value != "") && (displaynumber.value.length == 11) &&
+        ((displaynumber.value.slice(0, 3) == "070") ||
+            (displaynumber.value.slice(0, 3) == "080") ||
+            (displaynumber.value.slice(0, 3) == "090") ||
+            (displaynumber.value.slice(0, 3) == "081")
+        )
+    ) {
+        let gettt = JSON.parse(localStorage.getItem("accountbal"));
+        if (gettt.global <= 0.50) {
+            calculating.style.display = "none";
+            calculatingfirst.style.display = "none";
+            callopacity.style.display = "none";
+            forcalls.style.display = "none";
+            forcallnumbers.style.display = "block";
+            invalidresult.style.display = "block";
+            showsomeresult.innerHTML = "Your account balance is too low for this call. You can borrow airtime or call back later.";
+            callersno.innerHTML = displaynumber.value;
+            network.innerHTML = "Glo";
+        } else if (gettt.global != 0) {
+            calculating.style.display = "none";
+            calculatingfirst.style.display = "none";
+            audioElement = new Audio('ringtone.mp3');
+            secondphone.classList.add("mysecondphone");
+            audioElement.play();
+            timeout = setInterval(checktimeout, 1000);
+            forcallnumbers.style.display = "block";
+            forcalls.style.display = "none";
+            callersno.innerHTML = displaynumber.value;
+            network.innerHTML = "Glo";
+            secondphone.style.display = "block";
+            reccall.style.display = "block";
+            networktwo.innerHTML = "Glo";
+        }
+    } else if ((displaynumber.value != "") && (displaynumber.value.length == 14) &&
+        ((displaynumber.value.slice(0, 4) == "+234") ||
+            (displaynumber.value.slice(0, 4) == "+229")
+        )
+    ) {
+        let gettt = JSON.parse(localStorage.getItem("accountbal"));
+        if (gettt.global <= 0.50) {
+            calculating.style.display = "none";
+            calculatingfirst.style.display = "none";
+            callopacity.style.display = "none";
+            forcalls.style.display = "none";
+            forcallnumbers.style.display = "block";
+            invalidresult.style.display = "block";
+            showsomeresult.innerHTML = "Your account balance is too low for this call. You can borrow airtime or call back later.";
+            calculatingfirst.style.display = "none";
+            callersno.innerHTML = displaynumber.value;
+            network.innerHTML = "Glo";
+        } else if (gettt.global != 0) {
+            calculating.style.display = "none";
+            calculatingfirst.style.display = "none";
+            audioElement = new Audio('ringtone.mp3');
+            secondphone.classList.add("mysecondphone");
+            audioElement.play();
+            timeout = setInterval(checktimeout, 1000);
+            forcallnumbers.style.display = "block";
+            forcalls.style.display = "none";
+            callersno.innerHTML = displaynumber.value;
+            network.innerHTML = "Glo";
+            secondphone.style.display = "block";
+            reccall.style.display = "block";
+            networktwo.innerHTML = "Glo";
+        }
+    } else if ((displaynumber.value != "") && (displaynumber.value.length == 11) &&
+        ((displaynumber.value.slice(0, 3) != "070") ||
+            (displaynumber.value.slice(0, 3) != "080") ||
+            (displaynumber.value.slice(0, 3) != "090") ||
+            (displaynumber.value.slice(0, 3) != "081")
+        )
+    ) {
+        callopacity.style.display = "none";
+        forcalls.style.display = "none";
+        forcallnumbers.style.display = "block";
+        invalidresult.style.display = "block";
+        showsomeresult.innerHTML = "This number does not exist.please, check the number and try again later.";
+        calculatingfirst.style.display = "none";
+        callersno.innerHTML = displaynumber.value;
+        network.innerHTML = "Glo";
+    } else {
+        callopacity.style.display = "none";
+        forcalls.style.display = "none";
+        forcallnumbers.style.display = "block";
+        invalidresult.style.display = "block";
+        showsomeresult.innerHTML = "This number does not exist.please, check the number and try again later.";
+        calculatingfirst.style.display = "none";
+        callersno.innerHTML = displaynumber.value;
+        network.innerHTML = "Glo";
+    }
+}
+
+// END OF CODES TO LOAD GLO CARD
