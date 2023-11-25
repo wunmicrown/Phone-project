5cf658a2635364056d6180e10f768de4ca8f7cba
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-11-25 16:32:37 +0100

    Beginning_of_commit_messege of call network

--- call.js
+++ call.js
@@ -1,62 +1,8 @@
-const calWork =(digit)=> {
-    showdisplay.innerHTML += digit;
+if (localStorage.accountbal == null) {
+    localStorage.setItem("accountbal", "");
 }
+var allBalance = { mtnbal: 0, airtelbal: 0, global: 0, mobile: 0 };
 
-const onOperator = (operator)=> {
-    showdisplay.innerHTML += operator;
-}
-const equalTo =()=> {
-
-    let k = showdisplay.innerHTML;
-    let i = showresult.innerHTML;
-
-    if (k.search("sin") == -1 && k.search("cos") == -1 && k.search("tan") == -1 && k.search("log") == -1  && k.search("√") == -1) {
-    eval(showdisplay.innerHTML);
-    showresult.innerHTML = eval(showdisplay.innerHTML); 
-    } else if (k.search("sin") != -1) {
-        const dd = k.replace("sin", "(Math.PI/180)*");
-        const tt = eval(dd);
-        const xx = eval(Math.sin(tt));
-        const gg = xx.toPrecision(4);
-        showresult.innerHTML = gg;
-    } else if (k.search("cos") != -1) {
-        const dd = k.replace("cos", "(Math.PI/180)*");
-        const tt = eval(dd);
-        const xx = eval(Math.sin(tt));
-        const gg = xx.toPrecision(4);
-        showresult.innerHTML = gg;
-    } else if (k.search("tan") != -1) {
-        const dd = k.replace("tan", "(Math.PI/180)*");
-        const tt = eval(dd);
-        const xx = eval(Math.sin(tt));
-        const gg = xx.toPrecision(4);
-        showresult.innerHTML = gg;
-    }  else if (k.search("log") != -1) {
-        const dd = k.replace("log", "Math.log10");
-        const tt = eval(dd);
-        const gg = tt.toPrecision(4);
-        showresult.innerHTML = gg;
-    } else if (k.search("√") != -1) {
-        const dd = k.replace("√", "Math.sqrt");
-        const tt = eval(dd);
-        const gg = tt.toPrecision(4);
-        showresult.innerHTML = gg;
-    }
-}
-const clean =()=> {
-    showdisplay.innerHTML = ""
-    showresult.innerHTML = "0"
-}
-const onOff=()=> {
-    if (btn2.innerHTML == "ON") {
-        btn2.innerHTML = "OFF"
-        showdisplay.innerHTML = " "
-        showresult.innerHTML = "0"
-        showresult.style.display = "block"
-        showdisplay.style.display = "block"
-    } else {
-        btn2.innerHTML = "ON"
-        showdisplay.style.display = "none"
-        showresult.style.display = "none"
-    }
+if (localStorage.accountbal == "") {
+    localStorage.setItem("accountbal", JSON.stringify(allBalance));
 }
