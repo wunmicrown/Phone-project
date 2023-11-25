e7821466a9d33192c6d017a30153afddccba8df5
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-11-25 22:15:11 +0100

    added an else if statement for the condition of my input

--- voulcher.js
+++ voulcher.js
@@ -6,3 +6,28 @@ const generate=()=>{
         array = []
     }
 }
+if (nocard.value == "" && selectcard.value == "Select Card" && amount.value == "Select Amount") {
+    infogenerate.style.display = "block";
+    infogenerate.innerHTML = `<div class="bg-secondary mt-5 pt-2 h6 text-center text-white h-50 rounded">Please fill out the empty spaces <br>
+            <button class="btn bg-white w-50 mb-3 p-0 mt-3" onclick="delInfo()">OK</button></div>`
+} else if (nocard.value != "" && selectcard.value == "Select Card" && amount.value == "Select Amount") {
+    infogenerate.style.display = "block";
+    infogenerate.innerHTML = `<div class="bg-secondary mt-5 pt-2 h6 text-center text-white h-50 rounded">Please fill out the remaining empty spaces <br>
+            <button class="btn bg-white w-50 mb-3 p-0 mt-3" onclick="delInfo()">OK</button></div>`
+    return;
+}else if (nocard.value != "" && selectcard.value != "Select Card" && amount.value == "Select Amount") {
+    infogenerate.style.display = "block";
+    infogenerate.innerHTML = `<div class="bg-secondary mt-5 pt-2 h6 text-center text-white h-50 rounded">Please fill out the remaining empty spaces <br>
+            <button class="btn bg-white w-50 mb-3 p-0 mt-3" onclick="delInfo()">OK</button></div>`
+    return;
+}else if (nocard.value != "" && selectcard.value == "Select Card" && amount.value != "Select Amount") {
+    infogenerate.style.display = "block";
+    infogenerate.innerHTML = `<div class="bg-secondary mt-5 pt-2 h6 text-center text-white h-50 rounded">Please fill out the remaining empty spaces <br>
+            <button class="btn bg-white w-50 mb-3 p-0 mt-3" onclick="delInfo()">OK</button></div>`
+    return;
+} else if (nocard.value == "" && selectcard.value != "Select Card" && amount.value != "Select Amount") {
+    infogenerate.style.display = "block";
+    infogenerate.innerHTML = `<div class="bg-secondary mt-5 pt-2 h6 text-center text-white h-50 rounded">Please fill out the remaining empty spaces <br>
+            <button class="btn bg-white w-50 mb-3 p-0 mt-3" onclick="delInfo()">OK</button></div>`
+    return;
+}
