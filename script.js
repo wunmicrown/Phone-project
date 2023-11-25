fb54868946c08ff4f887df92094bf2f159872bc4
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-11-25 15:35:25 +0100

    added a triger function to music button

--- script.js
+++ script.js
@@ -17,18 +17,18 @@ displaydate.innerHTML = d.toDateString();
 var getTimeDate = () => {
     var j = new Date();
     let myhrs = j.getHours();
-    let mymin =  j.getMinutes();
+    let mymin = j.getMinutes();
     if (myhrs.toString().length < 2) {
         myhrs = "0" + hr;
-    } 
-    if (myhrs>12) {
-        myhrs = myhrs-12;
+    }
+    if (myhrs > 12) {
+        myhrs = myhrs - 12;
         displaytime.innerHTML = myhrs + ":" + mymin + " PM"
     } else {
-        displaytime.innerHTML = myhrs + ":" + mymin + " AM" ;
+        displaytime.innerHTML = myhrs + ":" + mymin + " AM";
     }
 }
-setInterval(function() {
+setInterval(function () {
     getTimeDate();
 }, 1000)
 
@@ -36,7 +36,7 @@ setInterval(function() {
 
 let checkOpen = 0;
 
-const openMe =()=> {
+const openMe = () => {
     checkOpen = 1;
     firstdisplay.style.display = "none";
     forcalls.style.display = "none";
@@ -46,17 +46,18 @@ const openMe =()=> {
     callopacity.style.display = "none";
     forcallnumbers.style.display = "none";
     forgame.style.display = "none";
+    forMusic.style.display = "none"
     forcal.style.display = "none";
 }
 
 
-const offMe =()=> {
+const offMe = () => {
     anotherinner.style.display = "none";
     firstdisplay.style.display = "block";
     checkOpen = 0;
 }
 
-const goBack=()=> {
+const goBack = () => {
     if (checkOpen == 0) {
         return;
     } else if (checkOpen == 1) {
@@ -69,10 +70,10 @@ const goBack=()=> {
         forgame.style.display = "none";
         forcal.style.display = "none";
     }
-    
+
 }
 
-const openGame=()=> {
+const openGame = () => {
     forgame.style.display = "block";
     forcalls.style.display = "none";
     anotherinner.style.display = "none";
@@ -81,8 +82,17 @@ const openGame=()=> {
     callopacity.style.display = "none";
     forcallnumbers.style.display = "none";
 }
+const openMusic = () => {
 
-const simSelect=()=> {
+    forMusic.style.display = "block";
+    forcalls.style.display = "none";
+    anotherinner.style.display = "none";
+    showbalance.style.display = "none";
+    infogenerate.style.display = "none";
+    callopacity.style.display = "none";
+    forcallnumbers.style.display = "none";
+}
+const simSelect = () => {
     if (displaynumber.value == "") {
         return;
     } else {
@@ -90,37 +100,37 @@ const simSelect=()=> {
         selectsim.style.display = "block";
         showbalance.style.display = "none";
     }
-    
+
 }
 
 
-const delInfo=()=> {
+const delInfo = () => {
     infogenerate.style.display = "none";
 }
 
 
-const displayCall=()=> {
-    forcalls.style.display = "block";	
+const displayCall = () => {
+    forcalls.style.display = "block";
 }
 
-const showNumbers=(num)=> {
+const showNumbers = (num) => {
     displaynumber.value += num;
 
 }
 
-const deleteMe=()=> {
-    displaynumber.value = displaynumber.value.substr(0,displaynumber.value.length-1)
+const deleteMe = () => {
+    displaynumber.value = displaynumber.value.substr(0, displaynumber.value.length - 1)
 }
 
-const deleteAll=()=> {
+const deleteAll = () => {
     displaynumber.value = "";
 }
 
-const delCallOpacity=()=> {
+const delCallOpacity = () => {
     callopacity.style.display = "none";
 }
 
-const cancelBalance=()=> {
+const cancelBalance = () => {
     callopacity.style.display = "none";
     displaynumber.value = "";
     showbalance.style.display = "none";
